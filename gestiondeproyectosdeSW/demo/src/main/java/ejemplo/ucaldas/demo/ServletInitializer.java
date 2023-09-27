a006ed20b85cb12800eb61f82547f062a1d2356c
Danny <dan21428@gmail.com>
2023-09-26 22:17:36 -0500

    pueba

--- gestiondeproyectosdeSW/demo/src/main/java/ejemplo/ucaldas/demo/ServletInitializer.java
+++ gestiondeproyectosdeSW/demo/src/main/java/ejemplo/ucaldas/demo/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package ejemplo.ucaldas.demo;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
