8b8d0577fac1b9ae69ae83ff9e9272e396aa3684
Danny <dan21428@gmail.com>
2023-09-28 01:21:06 -0500

    prueba12

--- gestiondeproyectosdeSW/demo/src/main/java/ejemplo/ucaldas/demo/DemoApplication.java
+++ gestiondeproyectosdeSW/demo/src/main/java/ejemplo/ucaldas/demo/DemoApplication.java
@@ -13,7 +13,7 @@ public class DemoApplication {
       SpringApplication.run(DemoApplication.class, args);
     }
     @GetMapping("/hello")
-    public String hello(@RequestParam(value = "name", defaultValue = "9999999999") String name) {
+    public String hello(@RequestParam(value = "name", defaultValue = "22222") String name) {
       return String.format("Hello %s!", name);
     }
 }
